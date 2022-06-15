8c16571e3671d0ffab4322ffc6675f5332a2a429
Hsu-ping <56856410+Hsu-ping@users.noreply.github.com>
2022-06-15 23:31:33 +0800

    Add files via upload

---
+++ mini-monopoly.cpp
@@ -0,0 +1,42 @@
+#include""
+#include""
+
+int main(){
+	printf("How many payers?(Maxinum:4)...>");
+	scanf("%d",&player);
+	if(玩家數量<1 || 玩家數量>4){
+		printf("only for 1-4 players\n");
+		reuturn 0;
+	}
+vector<Player> player_list;
+cin >> player_name;
+
+Player p(player_name);
+player_list.apush_back(p);
+
+
+do{
+    印出地圖()//冠宏
+    玩家丟骰子
+    檢查過原點獎勵(player)//冠宏
+    修改玩家位置(player,dice)//冠宏
+    if(檢查地是誰的()==NULL){
+        買or not 
+    }
+    else if(檢查地是誰的()==自己){
+        檢查是否可以升級()
+        升級or not 
+    }
+    else if (MapUnit() == J){
+        一回合不能動(player)
+    } 
+    else if (檢查地是誰的()!=自己){
+        罰錢(player) 
+        if(破產()){
+            出局(player) //冠宏
+            // 釋放全部土地() 並且土地等級歸1 for 棒子
+        }
+    }
+    
+    判斷是否結束
+}while(剩餘玩家人數 > 1)
