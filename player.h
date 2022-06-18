f7ce84736916c06b8a6f874aa4ec1f6a59cf7b79
LilDickYYder <107025478+LilDickYYder@users.noreply.github.com>
2022-06-18 22:51:37 +0800

    Update player.h

--- player.h
+++ player.h
@@ -1,132 +1,131 @@
 #ifndef PLAYERUNIT__
 #define PLAYERUNIT__
 
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
 
-constexpr int MAX_PLAYER_NUM = 4;
-constexpr char const *DEFAULT_PLAYER_NAME[MAX_PLAYER_NUM] = {"Player 1","Player 2","Player 3","Player 4"};
+constexpr int maxPlayerNum_ = 4;
+constexpr const char* defaultPlayerName[maxPlayerNum_] = {"Player 1", "Player 2", "Player 3", "Player 4"};
 
 class Player{
 	public:
-		Player( const int id , const std::string name ){
-			id_ = id;
-			money_ = initMoney;
-			position_ = 0;
-			prevPosition_ = 0;
-			inJail_ = false;
-			out_ = false;
-			name_ = name;
-		//	owned_city_ 
-		}
+		Player( 
+			int id , 
+			const std::string &name 
+		) : id_(id),
+			money_(initMoney),
+			position_(0),
+			inJail_(false),
+			//out_(false),
+			name_(name) {}
 		
-		void loseMoney( const int m ){
+		
+		void loseMoney( int m ){
+			// std::cout << money_ << std::endl;
 			money_ -= m;
+			// std::cout << money_ << std::endl;
 		}
 		
-		void gainMoney( const int m ){
+		void gainMoney( int m ){
 			money_ += m;
 		}
 		
-		void addCity( const int cityId ){
+		void addCity( int cityId ){
 			ownedCity_.push_back( cityId );
 		}
 		
-		void move( const int dicePoint , const int numCity ){ //需要傳入city的總數 
-			prevPosition_ = position_;
+		void move( int dicePoint , int numCity ){ //嚙豎要嚙褒入city嚙踝蕭嚙窯嚙踝蕭 
 			position_ += dicePoint;
 			if( position_ > numCity - 1 ) position_ = position_ - numCity - 1 ;	
 		}
 		
-		void setInJail( bool set ){ //傳入1進監獄 
-			inJail_ = set;
+		void setInJail(){ //嚙箠嚙褊綽蕭 
+			inJail_ = true;
 		}
 		
-		void setPlayerOut( bool set ){ //傳入1將player淘汰出局 
-			out_ = set;
+		void setOutJail(){ //嚙碼嚙褊綽蕭 
+			inJail_ = false;
 		}
 		
-		const bool isBankrup() const {
+		const bool isBankrupt() const {
 			return ( money_ < 0 ) ? true : false;
 		}
+				
+	//	const bool isFinishRound() const {  //嚙瞌嚙稻嚙緬嚙盤嚙踝蕭嚙瘢 
+	//		return ( prevPosition_ > position_ ) ? true : false;
+	//	} 
 		
-		const bool isFinishRound() const {  //是否通過原點 
-			return ( prevPosition_ > position_ ) ? true : false;
-		} 
-		
-		int getId() const {
+		const int getId() const {
 			return id_;
 		}
 		
-		int getMoney() const {
+		const int getMoney() const {
 			return money_;
 		}
 		
-		int getPosition() const {
+		const int getPosition() const {
 			return position_;
 		}
 		
-		bool isInJail() const {
+		const bool isInJail() const {
 			return inJail_;
 		}
 		
-		bool isOut() const {
+	/*	const bool isOut() const {
 			return out_;
-		}
+		}*/
 		
-		std::string getName() const {
+		const std::string getName() const {
 			return name_;
 		}
 		
-		std::vector<int> getOwnedCity() const {
+		const std::vector<int> getOwnedCity() const {
 			return ownedCity_;
 		}
 	
 	private:
-		int id_ = 0;
+		const int id_ = 0;
 		int money_ = 0;
 		int position_ = 0;
-		int prevPosition_ = 0;
 		bool inJail_ = false;
-		bool out_ = false;
-		std::string name_ ;
-		std::vector<int> ownedCity_; // 擁有的city 
+	//	bool out_ = false;
+		const std::string name_ ;
+		std::vector<int> ownedCity_; // 嚙誰佗蕭嚙踝蕭city 
 		constexpr static int initMoney = 25000;
 };
 
 class WorldPlayer{
 	public:
-		WorldPlayer( const int num ){
+		WorldPlayer( int num ){
 			numPlayer_ = num;
 			
-			for( int i=0 ; i<num ; ++i ){
+			for( int i = 0 ; i < num ; ++i ){
 				std::string name;
-				std::cout << "Please input player " << i+1 << "'s name (Default name is: " << DEFAULT_PLAYER_NAME[i] << "):";
+				std::cout << "Please input player " << i+1 << "'s name (Default name is: " << defaultPlayerName[i] << "):";
 				getline( std::cin , name );
-				
+				 
 				std::string playerName;
 				
-				if( name.empty() ) playerName = DEFAULT_PLAYER_NAME[i];
+				if( name.empty() ) playerName = defaultPlayerName[i];
 				else playerName = name;
 				
-				Player *playerNode;
-				playerNode = new Player( i , playerName );
-				player.push_back( playerNode );
+				player.push_back(new Player( i , playerName ) );
 			}			
 		//	std::cout << player[1]->getName() << std::endl;
 		//	std::cout << player[2]->getName() << std::endl;
 		}
 		
-		Player& getPlayerById( const int id ) const {
+		Player& getPlayerById( int id ) const {
+		//	std::cout << "getPlayerById" << std::endl;
 			return *player[id];
 		}
 		
-		void playerOut( const int id ){
-			player[id]->setPlayerOut(true); 
-		}
+/*		void playerOut( int id ){
+			player[id]->setPlayerOut(); 
+		} */
 		
-		int getNumPlayer() const {
+		const int getNumPlayer() const {
 			return numPlayer_;
 		}
 	private:
